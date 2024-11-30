104406d31ebaa4c3e31800e3960657cd666b15a4
misaka19683 <2398412341@qq.com>
2024-11-30 15:06:17 +0800

    use batch

--- src/nft.rs
+++ src/nft.rs
@@ -1,4 +1,4 @@
-use nftables::{expr::*, helper::apply_ruleset, schema::*, stmt::*, types::*};
+use nftables::{batch::Batch,expr::*, helper::apply_ruleset, schema::*, stmt::*, types::*};
 
 fn create_nftables_objects() -> (NfListObject, NfListObject, NfListObject) {
     // 创建 IPv6 表和链
@@ -32,44 +32,49 @@ fn create_nftables_objects() -> (NfListObject, NfListObject, NfListObject) {
     };
 
     (NfListObject::Table(table), NfListObject::Chain(chain), NfListObject::Rule(rule))
+    //(table, chain, rule);
 }
 
 // 执行多个 nftables 操作命令
-fn apply_nftables_action(actions: Vec<NfCmd>) -> Result<(), Box<dyn std::error::Error>> {
+fn apply_nftables_action(a:usize) -> Result<(), Box<dyn std::error::Error>> {
     // 将所有命令对象放入 nftables 对象中
-    let nftables = Nftables {
-        objects: actions.into_iter().map(NfObject::CmdObject).collect(),
-    };
+    // let nftables = Nftables {
+    //     objects: actions.into_iter().map(NfObject::CmdObject).collect(),
+    // };
 
+    let (table, chain, rule) = create_nftables_objects();
+    let mut batch = Batch::new();
+    if a==1 {
+        batch.add(table);
+        batch.add(chain);
+        batch.add(rule);
+    }else {
+        batch.delete(table);
+        batch.delete(chain);
+        batch.delete(rule);
+    };
+    let ruleset=batch.to_nftables();
+    apply_ruleset(&ruleset,None, None)?;
     // 应用 nftables 配置
-    apply_ruleset(&nftables, None, None)?;
+    //apply_ruleset(&nf, None, None)?;
 
     Ok(())
 }
 pub fn setup_nftables() -> Result<(), Box<dyn std::error::Error>> {
-    let (table_obj, chain_obj, nf_rule_obj) = create_nftables_objects();
 
-    // 构造操作命令：添加表、链和规则
-    let add_table = NfCmd::Add(table_obj);
-    let add_chain = NfCmd::Add(chain_obj);
-    let add_rule = NfCmd::Add(nf_rule_obj);
-
-    // 执行添加操作
-    apply_nftables_action(vec![add_table, add_chain, add_rule])?;
+    apply_nftables_action(1)?;
     
     Ok(())
 }
 
 pub fn delete_nftables() -> Result<(), Box<dyn std::error::Error>> {
-    let (table_obj, chain_obj, nf_rule_obj) = create_nftables_objects();
-
     // 构造操作命令：删除表、链和规则
-    let delete_table = NfCmd::Delete(table_obj);
-    let delete_chain = NfCmd::Delete(chain_obj);
-    let delete_rule = NfCmd::Delete(nf_rule_obj);
+    // let delete_table = NfCmd::Delete(table_obj);
+    // let delete_chain = NfCmd::Delete(chain_obj);
+    // let delete_rule = NfCmd::Delete(nf_rule_obj);
 
     // 执行删除操作
-    apply_nftables_action(vec![delete_table, delete_chain, delete_rule])?;
-    
+    //apply_nftables_action(vec![delete_table, delete_chain, delete_rule])?;
+    apply_nftables_action(0)?;
     Ok(())
 }
