8cd64b37effa469de4781de98255d15415bcacf0
misaka19683 <2398412341@qq.com>
2024-12-04 16:06:23 +0800

    nothing worth mentioning

--- src/globals.rs
+++ src/globals.rs
@@ -1,13 +1,12 @@
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 use ipnet::Ipv6Net;
-use pnet::datalink::*;
+use pnet::datalink::{interfaces,NetworkInterface};
 //{interfaces, NetworkInterface};
 // 全局变量定义
 pub static QUEUE_NUM: u16 = 0;
 pub static GLOBAL_CONTAINER: Lazy<Mutex<Vec<Ipv6Net>>> = Lazy::new(|| Mutex::new(Vec::new()));
 pub static INTERFACE_NAME: Lazy<Mutex<Option<NetworkInterface>>> = Lazy::new(|| Mutex::new(None));
-//pub static INTERFACE_LIST: Lazy<Mutex<Vec<NetworkInterface>>> = Lazy::new(|| Mutex::new(Vec::new()));
 
 // GLOBAL_CONTAINER 方法
 /// 向全局容器添加一个截断后的 IPv6 网络。
@@ -30,16 +29,16 @@ pub fn clear_container() {
 
 // INTERFACE_NAME 方法
 /// 设置全局接口名称。
-pub fn set_interface_name(interface_name: String) {
-    let interface_names_match=|iface:&NetworkInterface| iface.name == interface_name;
+pub fn set_interface_name(input_interface_name: String) {
+    let interface_names_match=|iface:&NetworkInterface| iface.name == input_interface_name;
     let interfaces= interfaces();
     let interface=
-    interfaces.into_iter()
+        interfaces.into_iter()
                 .filter(interface_names_match)
                 .next()
                 .expect("Interface not found");
-    let mut contain = INTERFACE_NAME.lock().unwrap();
-    *contain = Some(interface);
+    let mut interface_name = INTERFACE_NAME.lock().unwrap();
+    *interface_name = Some(interface);
 }
 
 /// 获取全局接口名称。
