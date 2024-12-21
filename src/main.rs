91880b70fdf49db9fb5c59af067dcbc7184f8609
CalunVier <CalunVier@outlook.com>
2024-12-21 19:46:35 +0800

    Optimizing the command line

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,20 @@ use IPv6PrefixFilter::daemon;
 
 use IPv6PrefixFilter::{ master::*,globals::*};
 
-/// 程序的命令行参数结构体
+/// Use IPv6PrefixFilter [COMMAND] --help to see the detail help for each subcommand.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {   
     #[command(subcommand)]
     command: Option<Commands>,
 
-    /// Specify the allowed IPv6 prefixes. Multiple prefixes can be allowed by repeating the `-p` option.
-    #[arg(short = 'p', long, value_parser = clap::value_parser!(Ipv6Net))]
-    ipv6_prefixes: Vec<Ipv6Net>,
-
-    /// Specify the wan interface.
-    #[arg(short = 'i', long)]
-    interface: Option<String>,
-
-    /// Enable blacklist mode. Prefixes specified with `-p` will be blocked.
-    #[arg(short = 'b', long)]
-    blacklist_mode: bool,
+    // /// Specify the allowed IPv6 prefixes. Multiple prefixes can be allowed by repeating the `-p` option.
+    // #[arg(short = 'p', long, value_parser = clap::value_parser!(Ipv6Net))]
+    // ipv6_prefixes: Vec<Ipv6Net>,
 
     /// Display detailed runtime information. The default log level is warning. Use -v to set to info, and -vv for debug.
     #[arg(short = 'v', long, action = clap::ArgAction::Count)]
-    verbose: u8,
-
-    /// Disable the feature of auto set nftables rules.
-    #[arg(long = "disable-nft-autoset")]
-    disable_nft_autoset: bool,
+    verbose: u8
 }
 
 /// 定义程序支持的命令
@@ -45,7 +33,16 @@ pub enum Commands {
     Run {
         /// Specify the allowed IPv6 prefixes. Multiple prefixes can be allowed by repeating the `-p` option.
         #[arg(short = 'p', long, value_parser = clap::value_parser!(Ipv6Net))]
-        ipv6_prefix: Option<Ipv6Net>,
+        ipv6_prefixs: Vec<Ipv6Net>,
+        /// Specify the wan interface.
+        #[arg(short = 'i', long)]
+        interface: Option<String>,
+        /// Enable blacklist mode. Prefixes specified with `-p` will be blocked.
+        #[arg(short = 'b', long)]
+        blacklist_mode: bool,
+        /// Disable the feature of auto set nftables rules.
+        #[arg(long = "disable-nft-autoset")]
+        disable_nft_autoset: bool
     },
     // 清理nftables规则
     /// Clear the nft rules set by the program, especially when the program exits improperly without executing the cleanup process.
@@ -81,17 +78,6 @@ fn main() {
             debug!("Logging level has been set to info.")
         }
     };
-    
-    let prefixs=args.ipv6_prefixes;
-
-    for prefix in prefixs.iter() {
-        add_to_container(*prefix);
-    }
-
-    if let Some(interface)= args.interface{
-        set_interface_name(interface);
-    };
-    
 
     // if let black_prefixes=args.blacklist_mode_prefixes{
     //     let mut flag=BLACKLIST_MODE.lock().unwrap();
@@ -103,10 +89,21 @@ fn main() {
 
     // 根据命令执行不同操作
     match args.command {
-        Some(Commands::Run { ipv6_prefix }) => {
+        Some(Commands::Run { ipv6_prefixs, interface, blacklist_mode, disable_nft_autoset}) => {
+            debug!("Running with prefix: ");
 
-            debug!("Running with prefix: {}", ipv6_prefix.unwrap());
-            add_to_container(ipv6_prefix.unwrap());
+            for prefix in ipv6_prefixs.iter() {
+                add_to_container(*prefix);
+            }
+
+            if let Some(interface)= interface{
+                set_interface_name(interface);
+            };
+
+            // TODO: Add blacklist_mode to global.
+
+            // TODO: disable_nft_autoset
+
             handle_run();
         }
         Some(Commands::Clear) => {
@@ -129,10 +126,6 @@ fn main() {
         Some(Commands::EmptyList)=>{
             clear_container();
         }
-
-        // None => {
-        //     println!("No command provided. Use --help for help.");
-        // }
         _ => {
             println!("No command provided. Use --help for help.");
         }
