110d9b772ea2eca40ef58baa26771d65a4c9b3fc
CalunVier <CalunVier@outlook.com>
2024-12-21 17:18:43 +0800

    Fix: build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
-fn main() {
+#[cfg(target_os = "windows")]
+fn config() {
     println!("cargo:rustc-link-arg=/MT"); // 强制使用静态运行时库
+}
+
+#[cfg(target_os = "linux")]
+fn config() {}
+
+fn main() {
+    config();
+
 }
