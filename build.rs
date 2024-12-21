0f38aa394f989859ea79c52c4f2e3fff89488eaf
CalunVier <CalunVier@outlook.com>
2024-12-21 18:31:46 +0800

    Cancel the compile option `cargo:rustc-link-arg=/MT`

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 #[cfg(target_os = "windows")]
 fn config() {
-    println!("cargo:rustc-link-arg=/MT"); // 强制使用静态运行时库
+    // println!("cargo:rustc-link-arg=/MT"); // 强制使用静态运行时库
 }
 
 #[cfg(target_os = "linux")]
